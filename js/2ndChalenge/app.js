c9ea68ce8e77f7b2a770ac9443a1d5688a6721bc
Emmet <emmetlawton@gmail.com>
2019-05-03 19:22:05 +0100

    stalker challenge

---
+++ js/2ndChalenge/app.js
@@ -0,0 +1,6 @@
+var firstName = prompt("what is your first name?");
+var sirName = prompt("what is your sir name?");
+var age = prompt("what is your age?");
+
+alert("Your full name is " + firstName + " " + sirName +", and your age is " + age + ".");
+consolge.log("Your full name is " + firstName + " " + sirName +", and your age is " + age + ".");
