74fe83836f690faee2ecfba2ba5d3f069d1e8c33
Emmet <emmetlawton@gmail.com>
2019-05-08 15:22:13 +0100

    started objects

---
+++ js/objects/1object/app.js
@@ -0,0 +1,24 @@
+var someObject = {
+    name: "Hedwig",
+    age: 5,
+    color: "white",
+
+    friends: [
+        {name: "malfoy"},
+        {name: "crabbe"},
+        {name: "Goyle"}
+    ],
+
+    diet: food = {
+        prime: "mice",
+        secondary: "insects"
+    },
+    isEvil: true
+}
+
+console.log(someObject.name)
+
+console.log(someObject.friends[0].name)
+
+console.log(someObject.diet.prime)
+
