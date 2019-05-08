d8e0597a6b01b70d79662e2627ba275eb422abb1
Emmet <emmetlawton@gmail.com>
2019-05-08 16:32:05 +0100

    finished objects

---
+++ js/objects/3_this_objects/app.js
@@ -0,0 +1,11 @@
+var comments = {
+    data: ["Good job!","you the man","You're awesome"],
+    print: function(){
+        this.data.forEach(function(x){
+            console.log(x)
+        });
+    }
+};
+
+comments.print()
+
