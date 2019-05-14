4e8fddad7f86e3856a07f346149146553eae1f91
Emmet <emmetlawton@gmail.com>
2019-05-14 18:25:13 +0100

    todo list events

---
+++ js/advancedMTD/otherEventsTodo/app.js
@@ -0,0 +1,15 @@
+var allLis = document.querySelectorAll("li");
+
+for(var i = 0; i < allLis.length; i++){
+    allLis[i].addEventListener("mouseover", function(){
+       this.style.color = "green";
+    });
+
+    allLis[i].addEventListener("mouseleave", function(){
+        this.style.color = "black";
+    })
+
+    allLis[i].addEventListener("click", function(){
+        this.classList.toggle("done")
+    });
+}
