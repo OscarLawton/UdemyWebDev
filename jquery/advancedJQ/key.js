4234a3e88bba772fce3812e5aea0e306f7c93c42
Emmet <emmetlawton@gmail.com>
2019-05-27 17:46:21 +0100

    jq effects started

---
+++ jquery/advancedJQ/key.js
@@ -0,0 +1,5 @@
+console.log("this worked, thank the whole thing")
+
+$("input").keypress(function(){
+    console.log("You pressed a key")
+})
---
+++ jquery/advancedJQ/on.js
@@ -0,0 +1,8 @@
+$("h1").on("click",function(){
+    $(this).css("backgroundColor", "green");
+});
+
+
+$("h3").on("mouseenter", function(){
+    $(this).css("backgroundColor", "purple");
+});
